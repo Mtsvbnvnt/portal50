50e20187838fa814226be0404440e8a9834b33a0
Matías Villarroel Benavente <mvillarroelb@utem.cl>
2025-09-28 17:01:43 -0300

    chore: agregar archivos sensibles al .gitignore y eliminarlos del control de versiones

--- Backend_Portal50/src/config/firebase.ts
+++
@@ -1,9 +0,0 @@
-// src/config/firebase.ts
-import admin from 'firebase-admin';
-import * as serviceAccount from "../../serviceAccountKey.json";
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-});
-
-export default admin;
